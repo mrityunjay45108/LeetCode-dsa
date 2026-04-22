6a9b5574be65a32c011752ce2cae00a9bb5fc22a
mrityunjay45108 <kumarmrityunjay5210@gmail.com>
2026-04-23 01:03:23 +0530

    Time: 0 ms (100%), Space: 8.9 MB (67.44%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.cpp
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -1,21 +1,17 @@
 class Solution {
 public:
     int lengthOfLastWord(string s) {
-        
-        int count = 0;
-        int i = s.size() - 1;
+       int i=s.length()-1;
+       int count =0;
+       while(i>=0 && s[i]==' '){
+        i--;
+       }
+      while(i>=0 && s[i]!=' ')
+     {
+      count ++;
+       i--;
+}
+   return count;
 
-        // Step 1: ignore trailing spaces
-        while(i >= 0 && s[i] == ' ') {
-            i--;
-        }
-
-        // Step 2: count last word
-        while(i >= 0 && s[i] != ' ') {
-            count++;
-            i--;
-        }
-
-        return count;
     }
 };

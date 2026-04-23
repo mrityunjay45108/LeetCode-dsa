2ebe20ece2129745e7aa3167a4f85e63c90c6fe7
mrityunjay45108 <kumarmrityunjay5210@gmail.com>
2026-04-23 17:39:14 +0530

    Time: 35 ms (5.02%), Space: 46.5 MB (5.01%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    vector<string> result;
+
+    bool isValid(string str) {
+        int count = 0;
+
+        for(char ch : str) {
+            if(ch == '(')
+                count++;
+            else
+                count--;
+
+            // Ye important check hona chahiye
+            if(count < 0)
+                return false;
+        }
+
+        return count == 0;
+    }
+
+    void solve(string& curr, int n) {
+
+        if(curr.length() == 2*n) {
+            if(isValid(curr)) {
+                result.push_back(curr);
+            }
+            return;
+        }
+
+        curr.push_back('(');
+        solve(curr, n);
+        curr.pop_back();
+
+        curr.push_back(')');
+        solve(curr, n);
+        curr.pop_back();
+    }
+
+    vector<string> generateParenthesis(int n) {
+        string curr = "";
+        solve(curr, n);
+        return result;
+    }
+};
